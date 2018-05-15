a6d2191c50961fbbd36ba4c8a96eb3d1a5e72ec6
mrjasonweaver <indyplanets@gmail.com>
2018-05-15 09:27:12 -0500

    Adding app-shell and theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule, MatIconModule, MatInputModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './app-shell/header/header.component';
+import { LogoComponent } from './app-shell/logo/logo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      // routes
+      { path: '', pathMatch: 'full', component: DashboardComponent }
+    ], {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
