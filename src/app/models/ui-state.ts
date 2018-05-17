3b94a78e32ad335709acf214b97030b535ea9144
James Weaver <indyplanets@gmail.com>
2018-05-17 14:15:26 -0500

    styling up the panel

--- src/app/models/ui-state.ts
+++ src/app/models/ui-state.ts
@@ -1,9 +1,11 @@
 export interface IUiState {
   actionOngoing: boolean;
+  isSelected: boolean;
   message: string;
 }
 
 export const initialUiState: IUiState = {
   actionOngoing: false,
+  isSelected: false,
   message: ''
 };
