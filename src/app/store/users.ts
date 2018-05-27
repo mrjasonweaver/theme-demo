1678df5346cd3dbf7e19aac71e36ff6a18d8b636
mrjasonweaver <indyplanets@gmail.com>
2018-05-27 18:11:28 -0500

    expirementing with cached content

--- src/app/store/users.ts
+++ src/app/store/users.ts
@@ -6,13 +6,20 @@ import { UiStateStore } from './ui-state';
 import { MatSnackBar } from '@angular/material';
 import { map, debounceTime } from 'rxjs/operators';
 
+interface CacheContent {
+  expiry: number;
+  value: any;
+}
+
 @Injectable()
 export class UsersStore {
 
   private _usersObject: BehaviorSubject<any> = new BehaviorSubject({});
   public readonly usersObject: Observable<IUsersObject> = this._usersObject;
+  private cache: Map<string, CacheContent> = new Map<string, CacheContent>();
   private _userSelected: BehaviorSubject<any> = new BehaviorSubject({});
   public readonly userSelected: Observable<IUser> = this._userSelected;
+  readonly DEFAULT_MAX_AGE: number = 300000;
   config = { duration: 1500 };
 
   constructor(
@@ -51,18 +58,32 @@ export class UsersStore {
 
   loadUsers(userParams, selected) {
     const isSelected = selected !== '';
+    const params = userParams;
+    const key = Object.keys(params).map(x => params[x]).reduce((a, x) => a += x,'');
+    const expiry = Date.now() + this.DEFAULT_MAX_AGE;
     this.uiStateStore.startAction('Retrieving Users...', isSelected);
-    this.usersService.getUsers(userParams)
+    if (this.cache.has(key)) {
+      console.log(`has key ${key}`);
+      console.log('usersObject', this.cache.get(key).value);
+      console.log('selected User', this.cache.get(key).value.items.filter(x => x.id === +selected)[0]);
+      this._usersObject.next(this.cache.get(key).value);
+      this._userSelected.next(this.cache.get(key).value.items.filter(x => x.id === +selected)[0]);
+    } else {
+      this.usersService.getUsers(userParams)
       .subscribe(res => {
+        const value = res;
+        this.cache.set(key, { value, expiry });
         this._usersObject.next(res);
         this._userSelected.next(res.items.filter(x => x.id === +selected)[0]);
-        this.uiStateStore.endAction('Users retrieved', isSelected);
       },
         err =>  {
           this.uiStateStore.endAction('Error retrieving Users', isSelected);
           this.snackBar.open('No Users found', null, this.config);
         }
       );
+    }
+    console.log(this.cache);
+    this.uiStateStore.endAction('Users retrieved', isSelected);
   }
 
 }
