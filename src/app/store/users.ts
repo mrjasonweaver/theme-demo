9f4db8c6d90e534a604580d6fe8cfba60e153f80
mrjasonweaver <indyplanets@gmail.com>
2018-05-27 23:21:23 -0500

    clean up

--- src/app/store/users.ts
+++ src/app/store/users.ts
@@ -55,30 +55,36 @@ export class UsersStore {
     });
   }
 
+  loadUsersFromCache(key, selected, isSelected) {
+    this._usersObject.next(this.cache.get(key).value);
+    this._userSelected.next(this.cache.get(key).value.items.filter(x => x.id === +selected)[0]);
+    this.uiStateStore.endAction('Users retrieved', isSelected);
+  }
+
+  loadUsersFromNetwork(userParams, key, selected, isSelected) {
+    const expiry = Date.now() + this.DEFAULT_MAX_AGE;
+    this.usersService.getUsers(userParams).subscribe(res => {
+      const value = res;
+      this.cache.set(key, { value, expiry });
+      this._usersObject.next(res);
+      this._userSelected.next(res.items.filter(x => x.id === +selected)[0]);
+      this.uiStateStore.endAction('Users retrieved', isSelected);
+    },
+      err =>  {
+        this.uiStateStore.endAction('Error retrieving Users', isSelected);
+        this.snackBar.open('No Users found', null, this.config);
+      }
+    );
+  }
+
   loadUsers(userParams, selected) {
     const isSelected = selected !== '';
     const p = userParams;
     const key = Object.keys(p).map(x => p[x]).reduce((a, x) => a += x, '');
-    const expiry = Date.now() + this.DEFAULT_MAX_AGE;
     this.uiStateStore.startAction('Retrieving Users...', isSelected);
-    if (this.cache.has(key)) {
-      this._usersObject.next(this.cache.get(key).value);
-      this._userSelected.next(this.cache.get(key).value.items.filter(x => x.id === +selected)[0]);
-      this.uiStateStore.endAction('Users retrieved', isSelected);
-    } else {
-      this.usersService.getUsers(userParams).subscribe(res => {
-        const value = res;
-        this.cache.set(key, { value, expiry });
-        this._usersObject.next(res);
-        this._userSelected.next(res.items.filter(x => x.id === +selected)[0]);
-        this.uiStateStore.endAction('Users retrieved', isSelected);
-      },
-        err =>  {
-          this.uiStateStore.endAction('Error retrieving Users', isSelected);
-          this.snackBar.open('No Users found', null, this.config);
-        }
-      );
-    }
+    this.cache.has(key)
+      ? this.loadUsersFromCache(key, selected, isSelected)
+      : this.loadUsersFromNetwork(p, key, selected, isSelected);
   }
 
 }
